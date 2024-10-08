645cebbc323751cde4dd339afcb9a1457ae857ac
lucas-software <160547725+lucas-software@users.noreply.github.com>
2024-10-08 11:48:25 -0300

    Project created

---
+++ src/test/java/com/gerenciamento/gerencia/GerenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerenciamento.gerencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
