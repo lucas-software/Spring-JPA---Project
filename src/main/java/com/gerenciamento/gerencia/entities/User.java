95c5b8a6df022281a42f9d996318779cff06d781
lucas-software <160547725+lucas-software@users.noreply.github.com>
2024-10-09 11:58:28 -0300

    H2 DB configuration

--- src/main/java/com/gerenciamento/gerencia/entities/User.java
+++ src/main/java/com/gerenciamento/gerencia/entities/User.java
@@ -1,13 +1,19 @@
 package com.gerenciamento.gerencia.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name="tb_user")
 public class User implements Serializable{
     @Serial
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
     private String email;
