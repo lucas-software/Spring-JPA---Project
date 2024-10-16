ed6df9d0ce33530108b0cde87b602a908c443ebe
gabriellrmartins <gabriellrmartins@gmail.com>
2024-10-16 18:34:56 -0300

    testing bot

--- src/main/java/com/gerenciamento/gerencia/resource/OrderResource.java
+++ src/main/java/com/gerenciamento/gerencia/resource/OrderResource.java
@@ -1,15 +1,15 @@
 package com.gerenciamento.gerencia.resource;
 
-import com.gerenciamento.gerencia.entities.Order;
-import com.gerenciamento.gerencia.services.OrderService;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.gerenciamento.gerencia.entities.Order;
+import com.gerenciamento.gerencia.services.OrderService;
 
 @RestController
 @RequestMapping(value ="/orders")
@@ -22,11 +22,5 @@ public class OrderResource {
     public ResponseEntity<List> findAll(){
         List<Order> list = service.findAll();
         return ResponseEntity.ok().body(list);
-    }
-
-    @GetMapping(value="/{id}")
-    public ResponseEntity<Order> findById(@PathVariable Long id){
-        Order obj = service.findById(id);
-        return ResponseEntity.ok().body(obj);
     }
 }
